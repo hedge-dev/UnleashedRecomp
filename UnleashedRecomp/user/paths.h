9dcadb9e0633f35ef7f0c4a6fd1bfe8639fd7818
Darío <dariosamo@gmail.com>
2024-12-15 08:55:53 -0300

    Add Linux home path for GetUserPath(). (#36)

--- UnleashedRecomp/user/paths.h
+++ UnleashedRecomp/user/paths.h
@@ -12,14 +12,35 @@ inline std::filesystem::path GetUserPath()
     if (std::filesystem::exists("portable.txt"))
         return std::filesystem::current_path();
 
-    std::filesystem::path userPath{};
+    std::filesystem::path userPath;
 
-    // TODO: handle platform-specific paths.
+#if defined(_WIN32)
     PWSTR knownPath = NULL;
     if (SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, NULL, &knownPath) == S_OK)
         userPath = std::filesystem::path{ knownPath } / USER_DIRECTORY;
 
     CoTaskMemFree(knownPath);
+#elif defined(__linux__)
+    const char *homeDir = getenv("HOME");
+    if (homeDir == nullptr)
+    {
+        homeDir = getpwuid(getuid())->pw_dir;
+    }
+
+    if (homeDir != nullptr)
+    {
+        // Prefer to store in the .config directory if it exists. Use the home directory otherwise.
+        const std::string dirName = "." USER_DIRECTORY;
+        std::filesystem::path homePath = homeDir;
+        std::filesystem::path configPath = homePath / ".config";
+        if (std::filesystem::exists(configPath))
+            userPath = configPath / dirName;
+        else
+            userPath = homePath / dirName;
+    }
+#else
+    static_assert(false, "GetUserPath() not implemented for this platform.");
+#endif
 
     return userPath;
 }
