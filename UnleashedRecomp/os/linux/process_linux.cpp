2b6013ad26b91408122bfa8525fdba105509cc01
Sajid <sajidur78@gmail.com>
2025-01-28 15:05:09 +0600

    Linux SetWorkingDirectory

--- UnleashedRecomp/os/linux/process_linux.cpp
+++ UnleashedRecomp/os/linux/process_linux.cpp
@@ -29,10 +29,9 @@ std::filesystem::path os::process::GetWorkingDirectory()
     }
 }
 
-// TODO
 bool os::process::SetWorkingDirectory(const std::filesystem::path& path)
 {
-    return chdir(path.c_str()) == 0;
+    return chdir(path.u8string().c_str()) == 0;
 }
 
 bool os::process::StartProcess(const std::filesystem::path& path, const std::vector<std::string>& args, std::filesystem::path work)
