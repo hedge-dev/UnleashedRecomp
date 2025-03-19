d1e98f8091d5a8548628827f055dd3bf1ff1a669
Onaterdem <onaterdem@gmail.com>
2025-03-19 23:14:32 +0300

    Clamp deltaTime to a max value of 1/15f

--- UnleashedRecomp/ui/options_menu.cpp
+++ UnleashedRecomp/ui/options_menu.cpp
@@ -1050,7 +1050,7 @@ static void DrawConfigOption(int32_t rowIndex, float yOffset, ConfigDef<T>* conf
         }
         else if constexpr (std::is_same_v<T, float> || std::is_same_v<T, int32_t>)
         {
-            float deltaTime = ImGui::GetIO().DeltaTime;
+            float deltaTime = std::fmin(ImGui::GetIO().DeltaTime, 1f/15f);
 
             bool fastIncrement = isSlider && (leftIsHeld || rightIsHeld) && (time - g_lastTappedTime) > 0.5;
             bool isPlayIncrementSound = true;
